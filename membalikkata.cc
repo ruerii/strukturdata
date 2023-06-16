c000a5638ddd628c3a3ffec8bc3ad3c44e5bf7d7
ruerii <129568954+ruerii@users.noreply.github.com>
2023-06-16 22:26:13 +0700

    Create membalikkata.cc

---
+++ membalikkata.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h> 
+using namespace std;
+
+int main()
+{
+    
+    char word[30];
+    
+    cout << "\t ==============Membalikkan kata============== \n\n";
+    cout << "Masukkan kata : ";
+    cin >> word;
+    cout << "Setelah dibalik : ";
+    
+    for(int i = strlen(word) - 1; i >= 0; i--){
+        
+        cout << word[i]<<"";}
+}
